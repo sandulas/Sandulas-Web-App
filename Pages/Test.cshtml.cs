7089e0c7aa201c35338cf4c712880783a0c4e59a
Adrian Sandu <sandulas@gmail.com>
2018-12-16 14:38:20 +0200

    - Fixed the razor page, changed the development URL not to use https because the bot was not working on localhost https - When publish to Azure we get an internal server error caused by the .UseBotFramework() request pipeline (because the bot is not configured?)

--- Pages/Test.cshtml.cs
+++ Pages/Test.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AspNetCoreMvc.Pages
+namespace SandulasWebApp.Pages
 {
 	public class TestModel : PageModel
 	{
--- Startup.cs
+++ Startup.cs
@@ -89,11 +89,11 @@ namespace SandulasWebApp
 			//app.UseHttpsRedirection();
 			app.UseStaticFiles();
 			app.UseMvc(routes =>
-					{
-						routes.MapRoute(
-							name: "default",
-							template: "{controller=Home}/{action=Index}/{id?}");
-					});
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
 			app.UseBotFramework();
 		}
 	}
